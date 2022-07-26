b1ec8a7340c9def58116b0ac266d8ade8c09d80e
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-07-26 20:59:04 -0300

    Petiones basicas en servidor

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req, res) {
-    res.send('Hola');
+app.use(router);
+
+router.get('/', function (req, res) {
+    res.send('Hola desde get');
 });
+
+router.post('/', function (req, res) {
+    res.send('Hola desde post');
+});
+
+router.get('/mensaje', function (req, res) {
+    res.send('lista de mensajes');
+});
+
+router.post('/mensaje', function (req, res) {
+    res.send('Mensaje anadido');
+});
+
+router.delete('/mensaje', function (req, res) {
+    res.send('Mensaje borrado');
+});
+
+// app.use('/', function (req, res) {
+//     res.send('Hola');
+// });
 
 var port = 3000
 app.listen(port);
