b55ed8ebcfac4d0426f1c09d23de40eb2674a213
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-07-26 22:55:07 -0300

    Servir Html Statico

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ router.delete('/mensaje', function (req, res) {
 //     res.send('Hola');
 // });
 
+app.use('/app', express.static('public'));
+
 var port = 3000
 app.listen(port);
 console.log('La aplicacion esta corriendo en el puerto ' + port);
