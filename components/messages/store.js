1a2d83632a3583c4ce82cf30605695168c630f13
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-07-29 21:02:47 -0300

    Alta y consulta con MongoDb

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,12 +1,26 @@
-// Creamos un Mock  es una simulacion de datos
-const list = [];
+const db = require('mongoose');
+const Model = require('./model')
+
+//Coneccion base de datos
+
+db.Promise = global.Promise
+db.connect('mongodb+srv://db_garroyo:Inetum_2022@cluster0.ws0pipe.mongodb.net/telegrom', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+console.log('[db] Conectada con exito');
 
 function addMessage(message) {
-    list.push(message);
+    //list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
+
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    //return list;
+    const message = await Model.find()
+    return message;
 }
 
 module.exports = {
@@ -16,3 +30,5 @@ module.exports = {
     //update
     //delete
 }
+
+//mongodb+srv://<username>:<password>@cluster0.ws0pipe.mongodb.net/test
